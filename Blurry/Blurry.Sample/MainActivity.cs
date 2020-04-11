41d808499bc7c18a6687df3ffc523e547c28cd06
Alan Kharebov <ak61us@gmail.com>
2020-04-10 17:24:14 -0700

    Sample is working

--- Blurry/Blurry.Sample/MainActivity.cs
+++ Blurry/Blurry.Sample/MainActivity.cs
@@ -1,8 +1,13 @@
 ﻿using Android.App;
+using Android.Graphics;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
+using Android.Util;
+using Android.Views;
 using Android.Widget;
+using Blurry.Library;
+using Java.Lang;
 
 namespace Blurry.Sample
 {
@@ -15,7 +20,62 @@ namespace Blurry.Sample
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            FindViewById<View>(Resource.Id.button).Click += (s, e) =>
+            {
+                // var startMs = System.CurrentTimeMillis
+                Blurry2.With(this)
+                    .Radius(25)
+                    .Sampling(1)
+                    .Color(Color.Argb(66, 0, 255, 255))
+                    .Async()
+                    .Capture(FindViewById(Resource.Id.right_top))
+                    .Into(FindViewById<ImageView>(Resource.Id.right_top));
+
+                Blurry2.With(this)
+                    .Radius(10)
+                    .Sampling(8)
+                    .Async()
+                    .Capture(FindViewById(Resource.Id.right_bottom))
+                    .Into(FindViewById<ImageView>(Resource.Id.right_bottom));
+
+                Blurry2.With(this)
+                    .Radius(25)
+                    .Sampling(1)
+                    .Color(Color.Argb(66, 255, 255, 0))
+                    .Async()
+                    .Capture(FindViewById(Resource.Id.left_bottom))
+                    .Into(FindViewById<ImageView>(Resource.Id.left_bottom));
+
+                // Log.Debug(GetString(Resource.string.app_name), "TIME " + (System.currentTimeMillis() - startMs).toString() + "ms"));
+            };
+
+            var blurred = false;
+
+            FindViewById<View>(Resource.Id.button).LongClick += (sender, args) =>
+            {
+                if (blurred)
+                {
+                    Blurry2.Delete(FindViewById<ViewGroup>(Resource.Id.content));
+                }
+                else
+                {
+                    var startMs = JavaSystem.CurrentTimeMillis();
+                    Blurry2.With(this)
+                        .Radius(25)
+                        .Sampling(2)
+                        .Async()
+                        .Animate(500)
+                        .Onto(FindViewById<ViewGroup>(Resource.Id.content));
+                    Log.Debug(GetString(Resource.String.app_name),
+                        "TIME " + (JavaSystem.CurrentTimeMillis() - startMs).ToString() + "ms");
+                }
+
+                blurred = !blurred;
+                args.Handled = true;
+            };
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
