a0deeefaa6c829dccbba72d35026df1b53e2728d
Alan Kharebov <ak61us@gmail.com>
2021-01-24 16:24:04 -0800

    Updated name spaces

---
+++ jp.wasabeef.blurry.Library/Blurry.cs
@@ -0,0 +1,21 @@
+using Android.Content;
+using Android.Views;
+
+namespace jp.wasabeef.blurry
+{
+    public class Blurry
+    {
+        private static readonly string Tag = typeof(Blurry).Name;
+
+        public static Composer With(Context context)
+        {
+            return new Composer(context);
+        }
+
+        public static void Delete(ViewGroup target)
+        {
+            var view = target.FindViewWithTag(Tag);
+            if (view != null) target.RemoveView(view);
+        }
+    }
+}
